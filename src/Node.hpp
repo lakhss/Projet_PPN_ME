1b06fbcc8bb05cd1427ca87d033165c59d28672e
Masten-repo <ounnasmasten@gmail.com>
2025-12-16 02:58:58 +0100

    Banned raw pointers

--- src/Node.hpp
+++ src/Node.hpp
@@ -1,3 +1,4 @@
+#include <memory>
 #pragma once
 
 #ifndef NODE_HPP
@@ -8,11 +9,12 @@ struct Node {
     double value = 0.0;      // the mean of the performances in the node
     int feature_idx = -1;       // the index of the feature we are using to split this node, goes from value 0 to 10  
     double threshold = 0.0;  // the best value to give the feature to split the node 
-    int samples = 0; // value of the leaf
-    Node* left = nullptr; 
-    Node* right = nullptr;
+    //int samples = 0; // value of the leaf
 
-    ~Node() { delete left; delete right; }
+    std::unique_ptr<Node> left = nullptr; 
+    std::unique_ptr<Node> right = nullptr;
+
+    
 };
 
 #endif 
