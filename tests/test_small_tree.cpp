fa9f8e29a88c95b38b5447f2303ac13e6d03e1f8
djohram1 <guerroudjdjohra@gmail.com>
2025-12-17 18:19:52 +0100

    Ajout test small tree  avec GTest

--- tests/test_small_tree.cpp
+++ tests/test_small_tree.cpp
@@ -1,22 +1,29 @@
 #include "DecisionTreeRegressor.hpp"
-#include <cassert>
-#include <iostream>
+#include "DataLoader.hpp"
+#include <gtest/gtest.h>
+#include <cmath>
 
-int main() {
-    std::vector<std::vector<double>> X = {
-        {0.0}, {1.0}, {2.0}, {3.0}
-    };
-    std::vector<double> y = {0.0, 1.0, 2.0, 3.0};
 
-    DecisionTreeRegressor tree;
-    tree.max_depth = 3;
-    tree.min_samples_split = 1;
+TEST(DecisionTreeTest, SmallTreePrediction) {
+   std::vector<std::vector<double>> X = {{0.0}, {1.0}, {2.0}, {3.0}};
+   std::vector<double> y = {0.0, 1.0, 2.0, 3.0};
 
-    tree.fit(X, y);
 
-    double pred = tree.predict({2.0});
-    assert(std::abs(pred - 2.0) < 0.5);
+   DecisionTreeRegressor tree;
+   tree.max_depth = 3;
+   tree.min_samples_split = 1;
+   tree.fit(X, y);
 
-    std::cout << "test_small_tree réussi" << std::endl;
-    return 0;
+
+   double pred = tree.predict({2.0});
+   EXPECT_NEAR(pred, 2.0, 0.5);  // tolérance 0.5
 }
+
+
+int main(int argc, char **argv) {
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
+
+
+
