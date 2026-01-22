d6f9e5348d06bf422d3662e8883e15f60ac30bba
djohram1 <guerroudjdjohra@gmail.com>
2026-01-22 21:34:32 +0100

    Update unit tests

--- tests/test_dataloader.cpp
+++ tests/test_dataloader.cpp
@@ -2,19 +2,19 @@
 #include "DataLoader.hpp"
 #include <gtest/gtest.h>
 
-
+// Test pour le DataLoader
 TEST(DataLoaderTest, LoadCSV) {
    std::vector<std::vector<double>> X;
    std::vector<double> y;
 
-
+// Chargement des données depuis le fichier CSV
    DataLoader::load_csv("../datasets/15k_ga_adaptive.csv", X, y);
 
 
-   EXPECT_FALSE(X.empty());              
-   EXPECT_EQ(X.size(), y.size());        
+   EXPECT_FALSE(X.empty());// Vérifie que X n'est pas vide 
+   EXPECT_EQ(X.size(), y.size());// Vérifie que X et y ont le même nombre d'échantillons        
 
-
+// Vérifie que chaque échantillon a 10 caractéristiques
    for (const auto& row : X) {
        EXPECT_EQ(row.size(), 10);        
    }
@@ -22,8 +22,8 @@ TEST(DataLoaderTest, LoadCSV) {
 
 
 int main(int argc, char **argv) {
-   ::testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+   ::testing::InitGoogleTest(&argc, argv);// Initialisation de Google Test
+   return RUN_ALL_TESTS();// Exécution de tous les tests
 }
 
 
--- tests/test_mse.cpp
+++ tests/test_mse.cpp
@@ -2,31 +2,31 @@
 #include "DataLoader.hpp"
 #include <gtest/gtest.h>
 
-
+// Test pour le DecisionTreeRegressor
 TEST(DecisionTreeTest, FitAndPredict) {
    DecisionTreeRegressor tree;
 
-
+// Jeu de données simple
    std::vector<std::vector<double>> X = {
        {1,2,3,4,5,6,7,8,9,10},
        {1,2,3,4,5,6,7,8,9,11},
        {1,2,3,4,5,6,7,8,9,12}
 };
-   std::vector<double> y = {1.0, 2.0, 3.0};
+   std::vector<double> y = {1.0, 2.0, 3.0};// Valeurs cibles correspondantes
 
 
-    tree.fit(X, y);
-   double pred = tree.predict(X[0]);
+    tree.fit(X, y);// Entraînement de l'arbre
+   double pred = tree.predict(X[0]);// Prédiction pour le premier échantillon
 
 
-   EXPECT_GE(pred, 1.0);   
-   EXPECT_LE(pred, 3.0);    
+   EXPECT_GE(pred, 1.0);//s’assure que pred >= 1.0  
+   EXPECT_LE(pred, 3.0);//s’assure que pred <= 3.0  
 }
 
 
 int main(int argc, char **argv) {
-   ::testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+   ::testing::InitGoogleTest(&argc, argv);// Initialisation de Google Test
+   return RUN_ALL_TESTS();// Exécution de tous les tests
 }
 
 
